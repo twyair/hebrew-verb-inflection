81fbc72851e52346db475774bc09cefc48adf6a4
yair twito <yairt16@gmail.com>
2021-10-08 13:40:18 +0300

    implement `past2binyan()`

--- __init__.py
+++ __init__.py
@@ -28,19 +28,29 @@ class Pronoun(Enum):
 
 class Gizra(Enum):
     KFULIM = auto()
-    PY = auto()
-    AYIN_WAW = auto()
+    PE_YOD = auto()  # used only for PAAL
+    AYIN_WAW = auto()  # used only for PAAL
+    PAAL_PARADIGM_1 = auto()
+    PAAL_PARADIGM_2 = auto()
 
-    def from_root(root: str) -> set[Gizra]:
+    @staticmethod
+    def new(verb: str, binyan: Binyan, root: str) -> set[Gizra]:
         res = set()
-        if root.startswith("y"):
-            res.add(Gizra.PY)
+        if binyan == Binyan.PAAL and verb.startswith("y"):
+            res.add(Gizra.PE_YOD)
         if len(root) == 3 and root[1] == root[2]:
             res.add(Gizra.KFULIM)
-        if len(root) == 3 and root[1] == "w":
+        if binyan == Binyan.PAAL and len(root) == 3 and root[1] == "w":
             res.add(Gizra.AYIN_WAW)
+        if verb in PAAL_PARADIGM_1:
+            res.add(Gizra.PAAL_PARADIGM_1)
+        if verb in PAAL_PARADIGM_2:
+            res.add(Gizra.PAAL_PARADIGM_2)
         return res
 
+    @staticmethod
+    def from_str(s: str) -> set[Gizra]:
+        return {Gizra[x] for x in s.split("+")}
 
 CONSONANTS = tuple("QbvgGdDhwzj7ykxlmnsRpfZqrcStT")
 GRONIYOT = tuple("hjQR")
@@ -57,7 +67,17 @@ PRONOUN_FUTURE_PREFIX = {
     Pronoun.HEM: "y",
     Pronoun.HEN: "T",
 }
-ALTERNATIVE_PAAL = {'barA!x', 'nadA!j', 'qarA!n', 'laRA!7', 'cajA!d', 'ZadA!q', 'lahA!g', 'SajA!q', 'nacA!q', 'SagA!v', 'pacA!r', 'baZA!q', 'barA!d', 'caxA!n', '7ajA!n', 'pajA!t', 'cajA!r', 'majA!Z', 'qadA!c', 'nasA!R', 'majA!l', 'mahA!r', 'TacA!c', 'nazA!l', 'DajA!f', 'najA!t', 'GalA!d', 'pajA!r', '7ahA!r', 'DaRA!x', 'naRA!m', 'caQA!r', 'zaRA!q', 'naQA!m', 'caRA!7', 'kavA!d', 'mahA!l', 'lajA!c', 'ZaRA!q', 'rajA!v', 'GaRA!r', 'GaQA!l', 'qadA!m', 'rajA!f', 'naRA!l', 'bajA!r', 'lahA!v', 'nahA!m', 'DavA!q', 'sajA!v', 'laRA!z', 'GavA!r', 'majA!7', '7aRA!n', 'kaQA!v', 'TaQA!r', 'zaRA!f', 'qadA!r', 'basA!m', 'DalA!q', 'ZahA!r', 'baSA!m', 'raxA!v', '7aRA!m', 'ZajA!q', 'ravA!Z', 'baRA!7', 'maRA!l', 'samA!r', 'matA!q', 'TajA!m', 'rajA!Z', 'TahA!d', 'kajA!l', 'maRA!x', 'bajA!c', 'lajA!Z', 'ZamA!q', 'TajA!v', 'GajA!n', 'ZaRA!n', 'ragA!l', 'zahA!r', 'nagA!h', 'lajA!x', 'bagA!r', 'rajA!q', 'caQA!v', 'lajA!m', 'baQA!c', 'raRA!c', 'camA!n', 'cajA!7', 'DacA!n', 'caQA!n', 'ZaRA!r', 'caxA!r', 'najA!l', 'lavA!c', 'paZA!r', 'nahA!g', 'lahA!7', 'samA!q', 'saRA!d', 'cajA!q', 'raQA!m', 'kawA!Z', 'caQA!f', 'natA!r', 'ZalA!l', 'qaZA!r', 'ZaRA!f', 'DaQA!g', 'pahA!q', 'raRA!f', 'laRA!g', 'GaRA!l', 'maRA!d', 'calA!w', 'DajA!s', 'zajA!l', 'baRA!r', 'kaRA!s', 'cajA!f', 'naQA!Z', 'nasA!q', 'zaRA!m', 'qamA!l', 'paRA!r', 'bajA!l', 'kajA!c', 'sajA!7', 'paRA!m', 'ba7A!l', 'ragA!z', 'caQA!7', 'raqA!v', 'DaQA!v', 'naRA!r', 'rahA!7', 'bahA!r', 'ZahA!v', 'caxA!v', 'GajA!x', 'DahA!r', 'raRA!m', 'majA!q', 'laRA!s', 'GaRA!c', 'nasA!j', 'cafA!r', 'naQA!q', 'maRA!7', 'nahA!q', 'ratA!t', 'kacA!l', 'sajA!r', 'saxA!l', 'zahA!v', 'na7A!R', 'caxA!x', 'caQA!g', 'qa7A!n', 'DajA!q', 'paRA!l', 'qarA!v', 'sajA!f', 'rajA!m', 'najA!r', 'calA!m', 'cafA!l', 'pajA!s', 'rahA!v', 'TaQA!v', 'qarA!m', 'nacA!l', 'caQA!l', 'raRA!d', 'zaqA!n', 'baRA!l', 'naRA!Z', 'SaRA!r', 'nagA!R', 'raRA!Z', 'GahA!q', 'GahA!r', 'bacA!l', 'lamA!d', 'bajA!n', 'ragA!c', 'maQA!s', 'qacA!v', 'nafA!j', 'saQA!n', 'bahA!q', 'GadA!l', 'saRA!r', 'ra7A!v', 'qarA!c', 'TaxA!l', 'namA!x', 'rafA!d', 'naQA!f', 'pajA!d', 'laqA!j', 'TaQA!w', 'kacA!r', 'ZahA!l', 'ZaRA!d', 'rajA!c', 'caRA!r', 'raRA!v', 'TaQA!m'}
+PAAL_PARADIGM_1 = {
+    'RavA!c', 'QavA!d', 'caQA!7', 'QasA!f', 'hawa!H', 'nagA!f', 'baSA!m', 'halA!x', 'lahA!v', 'TaQA!m', 'ca!r', 'saRA!d', 'lajA!Z', 'maja!Q', 'QasA!r', 'caxA!n', 'ZajA!q', 'QahA!v', 'qa!Q', 'QarA!v', 'ZanA!n', 'macA!c', 'Sane!Q', 'naqA!m', 'caxA!v', 'lahA!g', 'Zame!Q', 'ZaRA!f', 'GaRA!c', 'paRA!l', 'nasA!R', 'yaqA!d', 'saxA!l', 'Qana!H', 'yaRA!f', 'male!Q', 'qarA!n', 'la!Z', 'DaQA!g', 'sajA!v', 'ja!l', 'jara!H', 'bajA!l', 'rajA!f', 'maRA!x', '7aRA!n', 'zaqA!n', 'qamA!l', 'ZaRA!n', 'caQA!l', 'kajA!l', 'baRA!l', 'QagA!f', 'Qara!H', 'nacA!v', 'yasA!d', 'natA!Z', 'me!t', 'raqA!v', 'GaQA!l', 'nasA!x', 'ra!c', 'qarA!v', '7ahA!r', 'natA!c', 'bara!Q', 'camA!n', 'jamA!Z', 'kavA!d', 'caxA!x', 'GahA!r', 'rajA!q', 'jazA!q', 'pajA!t', 'na7A!r', 'DajA!q', 'jafa!H', 'jarA!d', 'zahA!r', 'na7A!f', 'RacA!t', 'paRA!m', 'yaRA!Z', 'na7a!H', 'lA!R', 'bahA!q', 'ragA!l', 'Daca!Q', 'QarA!g', 'ca!t', 'haga!H', 'kacA!l', 'RacA!r', 'yavA!c', 'barA!d', 'navA!l', 'GaRA!l', 'qarA!m', 'Zava!Q', 'QaZA!l', 'Gama!Q', 'QagA!r', 'DavA!q', 'GavA!r', 'yare!Q', 'qacA!c', 'GahA!q', 'yarA!c', 'SagA!v', 'caRA!7', 'jasa!H', 'qarA!c', 'natA!z', 'caQA!f', '7ajA!n', 'rA!v', 'pajA!r', 'yacA!n', 'QamA!d', 'nafA!j', 'yaRA!d', 'QarA!z', 'DahA!r', 'sa!d', 'cajA!q', 'jaZa!H', 'qadA!r', 'naZA!r', 'QazA!r', 'nafA!l', 'ZaRA!d', 'rahA!v', 'laRA!7', 'pacA!r', 'lavA!c', 'naRA!l', 'majA!l', 'DA!q', 'pahA!q', 'sajA!r', 'nadA!r', 'GajA!x', 'DalA!q', 'ba7A!l', 'zaRA!m', '7ala!Q', 'cA!j', 'sava!Q', 'naRA!r', 'yada!H', 'caQA!g', 'QanA!q', 'caQA!v', 'yaqA!v', 'raRA!d', 'ha!m', 'QamA!r', 'naca!H', 'jasA!r', 'jada!H', 'ravA!Z', 'GadA!l', 'yara!H', 'calA!m', 'maRA!d', 'RA!z', 'nagA!h', 'maRA!l', 'najA!t', 'yazA!m', 'raQA!m', 'DajA!s', 'Qata!H', 'mA!x', 'jarA!Z', 'nagA!R', 'TaxA!l', 'cafA!r', 'rA!m', 'naSa!Q', 'yafa!H', 'DacA!n', 'janA!n', 'nasA!j', 'yaZa!Q', 'lahA!7', 'qacA!v', 'ba!Q', 'kacA!r', 'rafa!Q', 'yagA!R', 'ZaRA!q', 'SaRA!r', 'najA!r', 'Qala!H', 'laRA!s', 'sajA!f', 'ZahA!l', 'qadA!m', 'RacA!n', 'majA!q', 'QavA!s', 'raRA!v', 'Qava!H', 'nahA!g', 'jarA!v', 'raRA!f', 'majA!Z', 'sama!Q', 'bacA!l', 'QaZA!r', 'jaza!H', 'rA!n', 'bajA!c', 'qa7A!n', 'yaRA!7', 'ZaRA!r', 'raxA!v', 'zaRA!q', 'TajA!m', 'samA!r', 'kaRA!s', 'ZamA!q', 'zaRA!f', 'mahA!l', 'baRA!r', 'nahA!q', 'janA!f', 'yarA!7', 'la!n', 'TaQA!v', 'ra!v', 'laRA!z', 'paRA!r', 'caRA!r', 'QagA!d', 'cafA!l', '7aRA!m', 'naRA!Z', 'TahA!d', 'DaQA!v', 'na7A!R', 'kaQA!v', 'ZahA!v', 'naxA!s', 'rajA!m', 'rahA!7', 'DajA!f', 'DA!l', 'nadA!j', 'rajA!c', 'ja7a!Q', 'zahA!v', 'jawa!H', 'pazA!z', 'nagA!S', 'SajA!q', 'qafa!Q', 'yaZA!r', 'QaxA!l', 'sa!j', 'cajA!d', 'jaca!H', 'nadA!v', 'ra!r', 'saQA!n', 'Qa7A!m', 'barA!x', 'bahA!r', 'hadA!r', 'nagA!v', 'GamA!m', 'QamA!n', 'mahA!r', 'kawA!Z', 'RatA!q', 'kala!Q', 'baZA!q', 'QafA!f', 'paZA!r', 'GajA!n', 'na7A!c', 'ZahA!r', 'TaQA!r', 'cajA!7', 'raRA!m', 'TaQA!w', 'calA!w', 'ratA!t', 'QafA!d', 'baRA!7', 'laqA!q', 'yanA!q', 'yaRa!H', 'saRA!r', 'camA!m', 'sajA!7', 'yaZA!q', 'zajA!l', 'lamA!d', 'Sa!m', 'ragA!c', 'bajA!n', 'QazA!q', 'bajA!r', 'Qafa!H', 'maZa!Q', 'jacA!x', 'lajA!x', 'Sa!S', 'natA!q', 'hada!H', 'nahA!m', 'nazA!r', 'rajA!Z', 'Ga!j', 'naZa!H', 'havA!l', 'baQA!c', 'cajA!f', 'lajA!m', 'laRA!g', 'savA!v', 'GaRA!r', 'caQA!r', 'kajA!c', 'Ga!l', 'ragA!z', 'basA!m', 'yasA!r', 'pajA!s', 'qara!Q', 'TajA!v', 'raRA!c', 'haza!H', 'na7A!l', 'qaZA!r', 'QajA!z', 'ba!n', 'RacA!c', 'QanA!s', 'raRA!Z', 'Qa7A!r', 'baza!Q', 'yarA!q', '7ame!Q', 'hama!H', 'rafA!d', 'maQA!s', 'pajA!d', 'bagA!r', 'caQA!n', 'DaRA!x', 'samA!q', 'natA!n', 'QaxA!f', 'lajA!c', 'najA!l', 'na!r', 'jadA!l', 'GalA!d', 'ZadA!q', 'majA!7', 'QahA!d', 'QafA!Z',
+}
+# belong to 2+ paal paradigms:
+# {'jarA!r', 'DamA!m', 'jaqA!q', 'jafA!f', 'javA!v', 'GadA!d', 'zaqA!q', 'hamA!m', 'jaZA!Z', 'malA!l', 'QarA!r', 'ja7A!7', 'jagA!g', 'jacA!c', 'kasA!s', 'jaxA!x', }
+# {'sa!d', 'la!n', 'ca!r', 'ja!l'}
+PAAL_PARADIGM_2 = {'natA!r', 'TacA!c', 'jalA!d', 'jatA!m', 'jalA!f', 'jarA!7', 'jafA!S', 'jacA!r', 'jata!H', 'ja7A!f', 'jalA!v', 'jafA!t', 'janA!v', 'yadA!R', 'yarA!d', 'jarA!g', 'jamA!d', 'jaqA!r', 'janA!x', 'jalA!c', 'jatA!l', 'jamA!q', 'jazA!r', 'javA!7', 'javA!Z', 'laqA!j', 'nasA!q', 'jacA!v', 'yacA!v', 'jatA!f', 'janA!q', 'jaZA!v', 'nacA!l', 'jacA!d', 'ja7A!v', 'qA!d', 'jafA!z', 'naQA!Z', 'nazA!l', 'jacA!q', 'jaxA!r', 'qA!v', 'jamA!r', 'jarA!f', 'naQA!m', 'jasA!m', 'jatA!x', 'jafA!Z', 'jarA!z', 'haxA!r', 'TA!m', 'jalA!7', 'jatA!r', 'jaSA!x', 'jarA!t', 'jamA!s', 'javA!c', 'jaSA!f', 'jaZA!d', 'jasA!x', 'jalA!Z', 'yalA!d', 'jagA!r', 'jadA!r', 'hadA!x', 'naQA!f', 'javA!l', 'jalA!q', 'naQA!q', 'jasA!l', 'jamA!l', 'jafA!r', 'jala!H', 'Rada!H', 'nacA!q', 'RatA!r', }
+assert not (PAAL_PARADIGM_1 & PAAL_PARADIGM_2)
+
+AW = set()
+
 DAGESH_LENE = {
     "x": "k",
     "g": "G",
@@ -246,7 +266,7 @@ def inflect_future(base: str, binyan: Binyan, pronoun: Pronoun, gizra: set[Gizra
         if pronoun == Pronoun.ANI:
             if base[:2] == "oQ":
                 return "Qo" + base[2:]
-            if Gizra.PY in gizra:
+            if Gizra.PE_YOD in gizra:
                 return "Q" + base
             if base[0].islower():
                 return "Q" + ("e" if base[0] in "ie" else "a") + base[1:]
@@ -258,7 +278,7 @@ def inflect_future(base: str, binyan: Binyan, pronoun: Pronoun, gizra: set[Gizra
         base_at = base.replace("Á", "")
         if base_at.endswith("E!H"):
             base_at = base_at[:-3]
-        elif Gizra.PY in gizra:
+        elif Gizra.PE_YOD in gizra:
             base_at = re.sub(".!", "á" if base_at[-4] in GRONIYOT else "3" , base_at)
         elif Gizra.KFULIM in gizra and len(base_at) == 6:
             base_at = base_at[:-1] + ("" if base_at[-1] in GRONIYOT else "_") + add_dagesh_lene(base_at[-1])
@@ -283,6 +303,7 @@ def inflect_future(base: str, binyan: Binyan, pronoun: Pronoun, gizra: set[Gizra
         if pronoun in (Pronoun.ATEN, Pronoun.HEN):
             base = base.replace("Á", "")
             if Gizra.AYIN_WAW in gizra and base.endswith("j"):
+                AW.add(base)
                 return re.sub(".!", "A!", base) + "naH"
             if base.endswith("E!H"):
                 return base[:-3] + "E!YnaH"
@@ -370,9 +391,9 @@ def inflect_past(base: str, binyan: Binyan, pronoun: Pronoun, gizra: set[Gizra])
             return base + PRONOUN_PAST_SUFFIX[pronoun].replace("!", "")
         return re.sub(r"[Aae]!", "á" if base[-4] in GRONIYOT else "3", base) + PRONOUN_PAST_SUFFIX[pronoun]
 
-def past2future(base: str, binyan: Binyan) -> Optional[str]:
+def past2future(base: str, binyan: Binyan, gizra: set[Gizra]) -> Optional[str]:
     if binyan == Binyan.HITPAEL:
-        return "y" + base[1:]
+        return "y" + base[1:].replace("a!H", "E!H")
     if binyan in (Binyan.PIEL, Binyan.PUAL):
         base = "y3" + remove_dagesh_lene(base[0]) + re.sub("^[Ie]", "a" if re.search("e[rQ].!", base) else "A", base[1:])
         return base.replace("a!H", "E!H")
@@ -412,24 +433,85 @@ def past2future(base: str, binyan: Binyan) -> Optional[str]:
         return "y" + ("e" if base[0] in GRONIYOT + ("r", ) else "I_") + add_dagesh_lene(base[0]) + "a" + remove_dagesh_lene(base[1]) + ("E!H" if base.endswith("aH")  else ("A" if base[3] in "hjR" else "e") + "!" + base[3])
     if binyan == Binyan.PAAL:
         if (m := re.fullmatch(r"(.)a(.)A!(.)", base)):
-            if base in ALTERNATIVE_PAAL:
+            if Gizra.PAAL_PARADIGM_1 in gizra:
+                if m[1] in "QhRj":
+                    return "yE" + m[1] + "é" + m[2] + "o!" + m[3]
+                if m[1] == "n":
+                    return "yI" + (add_dagesh_forte(m[2]) if m[2] not in "QRjhr" else "n" + m[2]) + ("A" if m[2] in "hjR" or m[3] in "QRjh" else "o") + "!" + m[3]
+                if m[1] == "y":
+                    return "yi" + m[2] + "A!" + m[3]
+                if m[2] == m[3]:
+                    return "ya" + remove_dagesh_lene(m[1]) + "o!" + m[3]
                 return "yI" + remove_dagesh_lene(m[1]) + add_dagesh_lene(m[2]) + "A!" + m[3]
-            if m[1] in "Rj":
+            if Gizra.PAAL_PARADIGM_2 in gizra:
+                if m[1] in "RjQh":
+                    return "yA" + m[1] + add_dagesh_lene(m[2]) + "o!" + m[3]
+                if m[1] == "n":
+                    return "yI" + (add_dagesh_forte(m[2]) if m[2] not in "QRjhr" else "n" + m[2]) + "A!" + m[3]
+                if m[1] == "y":
+                    return "ye" + m[2] + ("A!" if m[3] in "Rjh" else "e!") + m[3]
+                # if m[1] in "Qh":
+                #     return "yE" + m[1] + add_dagesh_lene(m[2]) + "o!" + m[3]
+            if m[1] in "RjQh":
                 return "yA" + m[1] + "á" + m[2] + "o!" + m[3]
-            if m[3] in "hjR":
+            if m[2] in "QhjR" or m[3] in "QhjR":
                 return "yI" + remove_dagesh_lene(m[1]) + add_dagesh_lene(m[2]) + "A!" + m[3]
             return "yI" + remove_dagesh_lene(m[1]) + add_dagesh_lene(m[2]) + "o!" + m[3]
         if re.fullmatch(r".a!.", base):
-            return "ya" + remove_dagesh_lene(base[0]) + "u!" + base[-1]
+            if Gizra.PAAL_PARADIGM_1 in gizra:
+                return "ya" + remove_dagesh_lene(base[0]) + "i!" + base[-1] + ("Á" if base[-1] in "Rhj" else "")
+            return "ya" + remove_dagesh_lene(base[0]) + "u!" + base[-1] + ("Á" if base[-1] in "Rhj" else "")
+        if re.fullmatch(r".A!.", base):
+            if Gizra.PAAL_PARADIGM_1 in gizra:
+                return "ya" + remove_dagesh_lene(base[0]) + "o!" + base[-1] + ("Á" if base[-1] in "Rhj" else "")
+            if Gizra.PAAL_PARADIGM_2 in gizra:
+                return "yI" + add_dagesh_forte(base[0]) + "o!" + base[-1] + ("Á" if base[-1] in "Rhj" else "")
+            return "ye" + remove_dagesh_lene(base[0]) + "A!" + base[-1]
         if re.fullmatch(r".a.a!H", base):
+            if Gizra.PAAL_PARADIGM_1 in gizra:
+                if base[0] in "QhjR":
+                    return "yE" + base[0] + "é" + base[2] + "E!H"
+                if base[0] == "n":
+                    return "yI" + (add_dagesh_forte(base[2]) if base[2] not in "QRjhr" else "n" + base[2]) + "E!H"
+                if base[0] == "y":
+                    return "yi" + base[2] + "E!H"
+                assert False
+            if Gizra.PAAL_PARADIGM_2 in gizra:
+                if base[0] in "RjQh":
+                    return "yA" + base[0] + add_dagesh_lene(base[2]) + "E!H"
+            if base[0] in "RjQh":
+                return "yA" + base[0] + "á" + base[2] + "E!H"
             return "yI" + remove_dagesh_lene(base[0]) + add_dagesh_lene(base[2]) + "E!H"
+
+def past2binyan(verb: str, gizra: set[Gizra]) -> Binyan:
+    if re.fullmatch(".a.(A!.|a!H|a!Q)", verb):
+        return Binyan.PAAL
+    if re.fullmatch(".[aA]!.", verb):
+        return Binyan.PAAL
+    if Gizra.PAAL_PARADIGM_1 in gizra:
+        return Binyan.PAAL
+    if re.fullmatch("hI..(i!.Á?|a!H)", verb):  # must be before PIEL and HITPAEL
+        return Binyan.HIFIL
+    if re.fullmatch("h(eY?.|E[rjhQR]é?.|W.)(i!.Á?|a!H)|he.e!.Á?", verb):
+        return Binyan.HIFIL
+    if re.match("hI(t.|[csS]T|Z7|zD)", verb) or re.fullmatch("hI_[7TD]A_.(e!.Á?|a!H|a!Q)", verb):
+        return Binyan.HITPAEL
+    if re.fullmatch("nI..(A!.|a!Q)", verb) or re.fullmatch("n(I.|E[jRhQ]é?).a!H", verb) or re.fullmatch("nE[jRhQ]é?.(A!.|a!Q)", verb) or re.fullmatch("nW.A!.", verb) or re.fullmatch("nI[rjhRQ]A!.", verb):
+        return Binyan.NIFAL
+    if re.fullmatch(".(I.á?.|[Ie][jRQhr])(e!.Á?|a!H)", verb) or re.fullmatch(r".W(.)e!.Á?", verb):
+        return Binyan.PIEL
+    if re.fullmatch("h(U.|u).(A!.|a!H|a!Q)", verb):  # must be before PUAL
+        return Binyan.HUFAL
+    if re.fullmatch(".(U.á?.|[Uo][jRQhr])(A!.|a!H|a!Q)", verb) or re.fullmatch(r".W(.)A!\1", verb):
+        return Binyan.PUAL
+    return None
 
 def inflect(base: str, binyan: Binyan, root: str) -> dict[str, str]:
     res = {}
-    gizra = Gizra.from_root(root)
+    gizra = Gizra.new(base, binyan, root)
     for pronoun in Pronoun:
         res["past_" + pronoun.name] = fixup(inflect_past(base, binyan, pronoun, gizra))
-    future_base = past2future(base, binyan)
+    future_base = past2future(base, binyan, gizra)
     if future_base is None:
         return res
     for pronoun in Pronoun:
